6307d4fc533832512068b0bae10c1eaf147378b7
Allan Fernando <allanfarmelin2001@yahoo.com.br>
2020-09-14 16:43:41 -0300

    Teste pronto

--- src/main/kotlin/com/digitalhouse/cadastroalunos/Turma.kt
+++ src/main/kotlin/com/digitalhouse/cadastroalunos/Turma.kt
@@ -1,3 +1,11 @@
 package com.digitalhouse.cadastroalunos
 
-class Turma(override val nome: String, val curso: Curso) : INomeavel
+class Turma(override val nome: String, val curso: Curso) : INomeavel{
+    override fun toString(): String {
+        var `return` = "Turma: $nome\nCurso: ${curso.nome}\nProfessor: ${curso.professor.nome}\nAlunos:\n"
+        for(aluno in curso.listaAlunos){
+            `return` += "\t${aluno.nome} ${aluno.sobrenome}\n"
+        }
+        return `return`
+    }
+}
