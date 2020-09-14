cdac23bd29adcb8bdb8a0d84677158ccd3a9fcda
hyigor <48020648+hyigor@users.noreply.github.com>
2020-09-14 16:18:40 -0300

    Classe curso - update

--- src/main/kotlin/com/digitalhouse/cadastroalunos/Curso.kt
+++ src/main/kotlin/com/digitalhouse/cadastroalunos/Curso.kt
@@ -6,11 +6,13 @@ class Curso(override var nome: String):INomeavel {
 
     fun adicionarAulas(aula: Aula) = listaAulas.add(aula)
 
-    fun adicionarAulas(vararg aulas: Aula) = aulas.forEach { adicionarAulas(it) }
+    fun adicionarAulas(vararg aulas: Aula) {
+        listaAulas.addAll(aulas)
+    }
+    fun adicionarAlunos(aluno: Aluno) = listaAlunos.add(aluno)
 
-
-    fun adicionarAlunos(aluno: Aluno){
-        listaAlunos.add(aluno)
+    fun adicionarAlunos(vararg alunos: Aluno){
+        listaAlunos.addAll(alunos)
     }
 
 }
